ff7c24fd1f1bf6e0f894d72878872b82d153e017
cjweber5 <79528247+cjweber5@users.noreply.github.com>
2023-04-25 19:40:37 -0700

    Implemented Pause, Fast Forward, Rewind and FbF

--- ObjectTracking/model_data/Detector.py
+++ ObjectTracking/model_data/Detector.py
@@ -168,14 +168,34 @@ class Detector:
 				
 		
 		startTime = 0
-		for frame in frame_buffer:
+		paused = False
+		frame_idx = 0
+		while True:
+			frame = frame_buffer[frame_idx]
 			currentTime = time.time()
-			fps = 1/(currentTime - startTime)
+			fps = 1 / (currentTime - startTime)
 			startTime = currentTime
-			cv2.putText(frame, "FPS: " + str(int(fps)), (20,70), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0), 2)
+			cv2.putText(frame, "FPS: " + str(int(fps)), (20, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
 			cv2.imshow("Result", frame)
-			cv2.waitKey(20)
 
+			if not paused:
+				if frame_idx < len(frame_buffer) - 1:
+					frame_idx += 1
+
+			key = cv2.waitKey(20) & 0xFF
+			if key == ord("q"):
+				break
+			elif key == ord("p"):
+				paused = not paused
+			elif key == ord("r"):
+				frame_idx = max(0, frame_idx - 30)
+			elif key == ord("f"):
+				frame_idx = min(len(frame_buffer) - 1, frame_idx + 30)
+			elif key == ord(" "):
+				if frame_idx < len(frame_buffer) - 1:
+					frame_idx += 1
+				paused = True
+	
 			# CHECKS TO SEE IF WEBCAM IS ENABLED
 			if self.videoPath == 0:
 				key = cv2.waitKey(1) & 0xFF
@@ -212,67 +232,88 @@ class Detector:
 
 		# Initialize variables for calculating FPS
 		startTime = 0
-		objdetector = cv2.createBackgroundSubtractorMOG2(history=10, varThreshold=20)
+		paused = False
+		frame_idx = 0
 		# Loop through all frames in the video
 		while success:
-			# Calculate FPS for the current frame
-			currentTime = time.time()
-			fps = 1/(currentTime - startTime)
-			startTime = currentTime
-			mask = objdetector.apply(image)
-			# Run object detection on the current frame
-			classLabelIDs, confidences, bboxs = self.net.detect(image, self.confThreshold.get())
+			if not paused:
+				# Calculate FPS for the current frame
+				currentTime = time.time()
+				fps = 1/(currentTime - startTime)
+				startTime = currentTime
+				# Run object detection on the current frame
+				classLabelIDs, confidences, bboxs = self.net.detect(image, self.confThreshold.get())
 
-			# Convert the bounding boxes and confidence values to lists
-			bboxs = list(bboxs)
-			confidences = list(np.array(confidences).reshape(1, -1)[0])
-			confidences = list(map(float, confidences))
-			bboxIdx = cv2.dnn.NMSBoxes(bboxs, confidences, self.sThreshold.get(), self.nmsThreshold.get())
+				# Convert the bounding boxes and confidence values to lists
+				bboxs = list(bboxs)
+				confidences = list(np.array(confidences).reshape(1, -1)[0])
+				confidences = list(map(float, confidences))
+				bboxIdx = cv2.dnn.NMSBoxes(bboxs, confidences, self.sThreshold.get(), self.nmsThreshold.get())
 
-			# Draws bounding boxes around the detected objects in the image, along with the class labels and confidence scores.
-			if len(bboxIdx) != 0:
-				for i in range(0, len(bboxIdx)):
+				# Draws bounding boxes around the detected objects in the image, along with the class labels and confidence scores.
+				if len(bboxIdx) != 0:
+					for i in range(0, len(bboxIdx)):
 
-					bbox = bboxs[np.squeeze(bboxIdx[i])]
-					classConfidence = confidences[np.squeeze(bboxIdx[i])]
-					classLabelID = np.squeeze(classLabelIDs[np.squeeze(bboxIdx[i])])
-					classLabel = self.classesList[classLabelID]
-					classColor = [int(c) for c in self.colorList[classLabelID]]
+						bbox = bboxs[np.squeeze(bboxIdx[i])]
+						classConfidence = confidences[np.squeeze(bboxIdx[i])]
+						classLabelID = np.squeeze(classLabelIDs[np.squeeze(bboxIdx[i])])
+						classLabel = self.classesList[classLabelID]
+						classColor = [int(c) for c in self.colorList[classLabelID]]
 
-					displayText = "{}:{:.2f}".format(classLabel, classConfidence)
+						displayText = "{}:{:.2f}".format(classLabel, classConfidence)
 
-					x,y,w,h = bbox
+						x,y,w,h = bbox
 
-					cv2.rectangle(image, (x,y), (x+w, y+h), color = classColor, thickness = 2)
-					cv2.putText(image, displayText, (x, y-10), cv2.FONT_HERSHEY_PLAIN, 1, classColor, 2)
-					
-					lineWidth = min(int(w * .3), int(h * .3))
-
-					if self.bValue.get() == "Enabled":
-						# TOP LEFT CORNER OF BOUNDING BOX
-						cv2.line(image, (x,y), (x+lineWidth,y), classColor, thickness=4)
-						cv2.line(image, (x,y), (x,y+lineWidth), classColor, thickness=4)
-
-						# TOP RIGHT CORNER OF BOUNDING BOX
-						cv2.line(image, (x+w,y), (x+w-lineWidth,y), classColor, thickness=4)
-						cv2.line(image, (x+w,y), (x+w,y+lineWidth), classColor, thickness=4)
-
-						# BOTTOM LEFT CORNER OF BOUNDING BOX
-						cv2.line(image, (x,y+h), (x+lineWidth,y+h), classColor, thickness=4)
-						cv2.line(image, (x,y+h), (x,y+h-lineWidth), classColor, thickness=4)
-
-						# BOTTOM RIGHT CORNER OF BOUNDING BOX
-						cv2.line(image, (x+w,y+h), (x+w-lineWidth,y+h), classColor, thickness=4)
-						cv2.line(image, (x+w,y+h), (x+w,y+h-lineWidth), classColor, thickness=4)
-					else:
-						cv2.rectangle(image, (x,y), (x+w, y+h), color = classColor, thickness = 3)
+						cv2.rectangle(image, (x,y), (x+w, y+h), color = classColor, thickness = 2)
 						cv2.putText(image, displayText, (x, y-10), cv2.FONT_HERSHEY_PLAIN, 1, classColor, 2)
+						
 						lineWidth = min(int(w * .3), int(h * .3))
 
-			cv2.putText(image, "FPS: " + str(int(fps)), (20,70), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0), 2)
-			cv2.imshow("Result", image)
-			cv2.waitKey(20)
-			#cv2.imshow("Mask", mask)
+						if self.bValue.get() == "Enabled":
+							# TOP LEFT CORNER OF BOUNDING BOX
+							cv2.line(image, (x,y), (x+lineWidth,y), classColor, thickness=4)
+							cv2.line(image, (x,y), (x,y+lineWidth), classColor, thickness=4)
+
+							# TOP RIGHT CORNER OF BOUNDING BOX
+							cv2.line(image, (x+w,y), (x+w-lineWidth,y), classColor, thickness=4)
+							cv2.line(image, (x+w,y), (x+w,y+lineWidth), classColor, thickness=4)
+
+							# BOTTOM LEFT CORNER OF BOUNDING BOX
+							cv2.line(image, (x,y+h), (x+lineWidth,y+h), classColor, thickness=4)
+							cv2.line(image, (x,y+h), (x,y+h-lineWidth), classColor, thickness=4)
+
+							# BOTTOM RIGHT CORNER OF BOUNDING BOX
+							cv2.line(image, (x+w,y+h), (x+w-lineWidth,y+h), classColor, thickness=4)
+							cv2.line(image, (x+w,y+h), (x+w,y+h-lineWidth), classColor, thickness=4)
+						else:
+							cv2.rectangle(image, (x,y), (x+w, y+h), color = classColor, thickness = 3)
+							cv2.putText(image, displayText, (x, y-10), cv2.FONT_HERSHEY_PLAIN, 1, classColor, 2)
+							lineWidth = min(int(w * .3), int(h * .3))
+
+				cv2.putText(image, "FPS: " + str(int(fps)), (20,70), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0), 2)
+				cv2.imshow("Result", image)
+
+			key = cv2.waitKey(20) & 0xFF
+			if key == ord("q"):
+				break
+			elif key == ord("p"):
+				paused = not paused
+			elif key == ord("r"):
+				frame_idx = -30
+			elif key == ord("f"):
+				frame_idx = 30
+			elif key == ord(" "):
+				paused = not paused
+			
+			if not paused:
+				if frame_idx != 0:
+					cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) +frame_idx)
+					frame_idx = 0
+				success, image = cap.read()
+
+				if not success:  # If the video ends, set the video position to the last frame
+					cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) - 1)
+					success, image = cap.read()
 
 			# CHECKS TO SEE IF WEBCAM IS ENABLED
 			if self.videoPath == 0:
@@ -292,7 +333,6 @@ class Detector:
 				if key == ord("q"):
 					break
 
-			(success, image) = cap.read()
+			#(success, image) = cap.read()
 		cv2.destroyAllWindows()
 		cap.release()
-				
